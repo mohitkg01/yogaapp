f4d389168ccfea0892a41bfe75d73b9b5b41ab23
Mohit <mohitkg2601@gmail.com>
2024-01-31 12:02:43 +0530

    login authorisation is added with auth0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../Styles/Header.css';
+import { useAuth0 } from "@auth0/auth0-react";
+
 const Header = () => {
+  const { loginWithRedirect ,logout ,isAuthenticated,user} = useAuth0();
   return (
     <div className="head">
         <div className='h-left'>
@@ -10,9 +13,11 @@ const Header = () => {
         <div className='h-right'>
             <Link to="productid">Product</Link>
             <Link to="batchid">Batch</Link>
-            <Link to='login'>Login</Link>
+            {isAuthenticated && <span>{user.nickname}</span>}
+            {isAuthenticated ?(<Link onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</Link>)
+            :(<Link onClick={() => loginWithRedirect()}>Login</Link>)}
         </div>
-    </div>
+    </div> 
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+  <Auth0Provider
+      domain="dev-13mtljx5knd61cwb.us.auth0.com"
+      clientId="ufAHYayWy96mkATLioFR5mbJm097ZOgl"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  );
